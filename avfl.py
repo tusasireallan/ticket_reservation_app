d259952c6aba034d0f736141a49c57b8278eb927
skhumbuzomt <smakademkhulu@gmail.com>
2020-08-21 21:10:09 +0200

    skhu and anna

---
+++ avfl.py
@@ -0,0 +1,13 @@
+def availableflight():
+    queryinsert= "INSERT INTO flight(flightID,flightdate,destanation,arrivedate,price) VALUES(%s,%s,%s,%s,%s)"
+    fightID = int(input("enter Flightid"))
+    flightdate = int(input("enter Flifgt Date"))
+    arrivedate = input("enter arrive Date")
+    price = input("Enter price")
+    values = (flightID,flightdate,destanation,arrivedate,price)
+    mypoint.excute(queryinsert,values)
+    mypointer.execute(queryinsert,values)
+    mydatebase.commit()
+    print("flight details succsefully entred")
+
+availableflight()
