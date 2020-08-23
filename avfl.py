bad70ec310ce16f96812d85429c0b0f6df080afb
anne <annefasan001@gmail.com>
2020-08-23 18:01:15 +0200

    changes from wrong spelling

--- avfl.py
+++ avfl.py
@@ -1,13 +1,13 @@
 def availableflight():
     queryinsert= "INSERT INTO flight(flightID,flightdate,destanation,arrivedate,price) VALUES(%s,%s,%s,%s,%s)"
     fightID = int(input("enter Flightid"))
-    flightdate = int(input("enter Flifgt Date"))
+    flightdate = int(input("enter Flight Date"))
     arrivedate = input("enter arrive Date")
     price = input("Enter price")
     values = (flightID,flightdate,destanation,arrivedate,price)
     mypoint.excute(queryinsert,values)
     mypointer.execute(queryinsert,values)
     mydatebase.commit()
-    print("flight details succsefully entred")
+    print("flight details succsefully entered")
 
 availableflight()
